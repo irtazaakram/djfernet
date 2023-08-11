022009e0225e5880a5922f039cf68365f2d33c04
irtazaakram <irtaza.akram@arbisoft.com>
2023-08-11 10:25:46 +0500

    fix: lint

--- fernet_fields/test/settings/pg.py
+++ fernet_fields/test/settings/pg.py
@@ -1,16 +1,14 @@
-import platform
-
 from .base import *  # noqa
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # matches travis config
+        # matches github actions config
         'NAME': 'djftest',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        # 'PASSWORD': 'postgres',
+        # 'HOST': 'localhost',
         'TEST': {
             'NAME': 'djftest',
             'USER': 'postgres',
